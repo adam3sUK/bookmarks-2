f19887a219c0602d37d2fcabc7d76c1800a8acdf
Adam Harbisher <aharbisher@gmail.com>
2021-11-10 14:18:18 +0000

    added if to decide db to use

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -3,7 +3,11 @@ require 'pg'
 class Bookmarks
 
   def self.all
-    connection = PG.connect :dbname => 'bookmark_manager'
+    if (ENV['RACK_ENV'] = 'test') do
+      connection = PG.connect :dbname => 'bookmark_manager_test'
+    else
+      connection = PG.connect :dbname => 'bookmark_manager'
+    end
     result = connection.exec "SELECT * FROM bookmarks"
     result.map do |bookmark|
       bookmark['url']
